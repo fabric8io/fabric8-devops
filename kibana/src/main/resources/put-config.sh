9f9ebd39493a8b29a615aba8990be139742c07c4
James Strachan <james.strachan@gmail.com>
2016-03-08 20:40:04 +0000

    lets add a default search and visualisation of the git commits #5757

--- kibana/src/main/resources/put-config.sh
+++ kibana/src/main/resources/put-config.sh
@@ -18,6 +18,8 @@ done
 if ! [ $(curl -s -f -o /dev/null ${ELASTICSEARCH_URL}/.kibana) ]; then
     curl -s -f -XPUT -d@/index-pattern.json "${ELASTICSEARCH_URL}/.kibana/index-pattern/logstash-*"
     curl -s -f -XPUT -d@/fabric8-search.json "${ELASTICSEARCH_URL}/.kibana/search/Fabric8"
+    curl -s -f -XPUT -d@/git-commits-search.json "${ELASTICSEARCH_URL}/.kibana/search/git-commits"
+#    curl -s -f -XPUT -d@/git-commits-per-project-visualisation.json "${ELASTICSEARCH_URL}/.kibana/visualization/git-commits-per-project"
     curl -s -f -XPUT -d@/fabric8-dashboard.json "${ELASTICSEARCH_URL}/.kibana/dashboard/Fabric8"
     curl -s -f -XPUT -d@/kibana-config.json "${ELASTICSEARCH_URL}/.kibana/config/4.4.0"
 fi
