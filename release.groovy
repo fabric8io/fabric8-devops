3be22d57b3e19521ea8a0c51a06f4df0fbacedea
rawlingsj <rawlingsj80@gmail.com>
2017-09-18 16:58:46 +0100

    modules have been moved outside of this repo now so lets clean it up a bit

--- release.groovy
+++ release.groovy
@@ -4,7 +4,7 @@ def imagesBuiltByPipeline() {
 }
 
 def externalImages(){
-  return ['nginx-controller','alpine-caddy','hubot-irc','eclipse-orion','nexus','gerrit','fabric8-kiwiirc','brackets','jenkins-jnlp-client','taiga-front','taiga-back','hubot-slack','lets-chat','jenkins-docker','maven-builder','gogs','grafana']
+  return ['nginx-controller','alpine-caddy','hubot-irc','eclipse-orion','nexus','gerrit','fabric8-kiwiirc','brackets','jenkins-jnlp-client','taiga-front','taiga-back','hubot-slack','lets-chat','gogs','grafana']
 }
 
 def repo(){
